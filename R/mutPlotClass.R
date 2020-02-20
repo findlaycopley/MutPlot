40314c57e2a6b10a5dd39e6b8c8147cb880937ff
findlaycopley <findlaycopley@hotmail.co.uk>
2020-02-20 12:58:11 +0000

    Added a show method for the class
    Shows the final plot if it exists otherwise it will print some info

--- R/mutPlotClass.R
+++ R/mutPlotClass.R
@@ -20,6 +20,27 @@ MutationPlot <- setClass("MutationPlot",
                                    genelist="table",
                                    plotdata="list"))
 
+setMethod("show", "MutationPlot",
+          function(object) {
+                  if (is.ggplot(object@plots$FinalPlot)) {
+                          print(object@plots$FinalPlot)
+                  } else {
+                          cat("This is an instance of MutationPlot","\n")
+                          cat("Samples:",
+                              object@plotdata$SampleData$SampleCol %>%
+                                      levels(),
+                              "\n")
+                          cat("Genes:",
+                              object@plotdata$GeneData$GeneCol %>%
+                                      levels(),
+                              "\n")
+                          cat("Number of Mutations:",
+                              object@mutationData %>%
+                                      dim(.) %>%
+                                      .[1])
+                          }
+                  }
+          )
 
 MutTheme <- theme_classic() +
         theme(axis.text = element_text(size=8),
